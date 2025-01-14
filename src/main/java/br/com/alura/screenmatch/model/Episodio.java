2d2e6b5833ae9a7d268e63e2a7669b088825704b
Rafael Vinicius da Silva Basilio <rafaelbasilio2004@gmail.com>
2025-01-14 10:51:32 -0300

    Atualização

---
+++ src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -0,0 +1,81 @@
+package br.com.alura.screenmatch.model;
+
+
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numeroEpisodio;
+    private Double avaliacao;
+    private LocalDate datalancamento;
+
+    public Episodio(Integer numeroTemprada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemprada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numeroEpisodio = dadosEpisodio.numero();
+
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        }catch (NumberFormatException ex) {
+            this.avaliacao = 0.0;
+        }
+        try {
+            this.datalancamento =LocalDate.parse( dadosEpisodio.dataLancamento());
+        }catch (DateTimeException ex) {
+            this.datalancamento = null;
+        }
+
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDatalancamento() {
+        return datalancamento;
+    }
+
+    public void setDatalancamento(LocalDate datalancamento) {
+        this.datalancamento = datalancamento;
+    }
+
+    @Override
+    public String toString() {
+        return  "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numeroEpisodio=" + numeroEpisodio +
+                ", avaliacao=" + avaliacao +
+                ", datalancamento=" + datalancamento ;
+    }
+}
